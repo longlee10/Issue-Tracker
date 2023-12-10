8e8b72a3a9b2e8c8ebe47465a454e6e0d2dd6fa1
longquan98@gmail.com <longquan98@gmail.com>
2023-12-10 16:35:54 -0500

    Builiding the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return <Pagination itemCount={103} pageSize={10} currentPage={11} />;
 }
